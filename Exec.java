9bb77c88a02db197d523debb9129794749d50e17
topia13-3 <toubouryokouki3@gmail.com>
2018-07-31 12:58:10 +0900

    挨拶追加

--- Exec.java
+++ Exec.java
@@ -2,6 +2,8 @@ package exsample01;
 public class Exec {
 	public static void main(String[] args) {
 			System.out.println("おはよう");
+			System.out.println("おはいお");
+			System.out.println("おはおは");
 
 		}
 }
